5480b98caa67d93678386845cc574a1a4ebb397d
yar0-slav <hornster1@gmail.com>
2022-04-15 07:57:55 +0200

    sooo

--- pages/index.js
+++ pages/index.js
@@ -286,7 +286,7 @@ export async function getStaticProps() {
 
     return {
         props: {
-            images,
+            images: images || false,
             nextCursor: nextCursor || false,
             folders
         }
