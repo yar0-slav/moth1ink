680cbd85c6e2ed3b7ac3ac72b0857d04d9e668a7
yar0-slav <hornster1@gmail.com>
2022-04-21 20:54:49 +0200

    font add

--- components/fonts.js
+++ components/fonts.js
@@ -1,7 +1,7 @@
 
 const Fonts = () => (
     <style jsx global>{`
-    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;900&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Quicksand:wght@300&family=Square+Peg&display=swap');
   `}</style>
 )
 export default Fonts
