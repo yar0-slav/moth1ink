67fa4e50352e987bd9ed2a1a74300d40fdf829cb
yaro <hornster1@gmail.com>
2022-10-09 01:39:50 +0200

    removing zoom from images, bad UX

--- components/sections/gallerySection.js
+++ components/sections/gallerySection.js
@@ -4,8 +4,6 @@ import Image from 'next/image'
 import Masonry from 'react-masonry-css'
 
 import Lightbox from "yet-another-react-lightbox";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
-
 import { mapImageResources } from '../../lib/cloudinary'
 
 export default function ImageGallery({
